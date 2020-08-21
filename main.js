d26ba1b30db3f8496c2673675293534fa2b0e5db
niveus323 <niveus323@gmail.com>
2020-08-21 16:23:48 +0900

    express 적용 밑 pm2 환경에서의 문제 해결

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = normalizePort(process.env.PORT||'52484');
-app.set('port',port);
-app.use(express.static(path.join(__dirname,'/')));
+
+app.set('views',__dirname+'/views');
+app.set('view engine','ejs');
+app.engine('html',require('ejs').renderFile);
+app.use(express.static('public'));
+// app.use(express.static(path.join(__dirname,'/')));
 
 app.get('/',function(request,response){
     response.render('youtubeSectionPlayer.html');
 });
 
-var server = app.listen(PORT,()=>{
-    console.log('App is listening on port ${PORT}');
+var server = app.listen('52484',()=>{
+    console.log('App is listening on port 52484');
 });
